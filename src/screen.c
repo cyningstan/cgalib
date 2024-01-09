9af945e10d3f37619955274fdffe4fc0a915dc79
Damian Gareth Walker <damian@cyningstan.org.uk>
2024-01-09 11:47:32 +0000

    Hercules support added

--- src/screen.c
+++ src/screen.c
@@ -17,8 +17,99 @@
 #include "cgalib.h"
 
 /*----------------------------------------------------------------------
+ * Data Definitions.
+ */
+
+/** @var base Base address of graphics output. */
+static char far *base;
+
+/** @var swidth Physical screen width in bytes. */
+static int swidth;
+
+/** @var interleave Number of rows interleaved. */
+static int interleave;
+
+/* @var ymult Y coordinate multiplier for calculating pixel address. */
+static int ymult;
+
+/* @var ydiv Y coordinate divisor for calculating pixel address. */
+static int ydiv;
+
+/*----------------------------------------------------------------------
+ * Level 2 Functions.
+ */
+
+/**
+ * Set the screen parameters according to the screen mode. For some
+ * reason, if this is done within the set{,_hercules)_mode functions,
+ * the values get reset on leaving. So they're here in a separate
+ * function, to be called immediately before trying to update the
+ * screen if not already set. That ensures they'll be available at the
+ * point they are needed.
+ * @param screen The screen data.
+ */
+void set_screen_parameters (Screen *screen)
+{
+    /* Hercules screen parameters */
+    if (screen->mode == 7) {
+	base = (char far *) 0xb0000221;
+	swidth = 90;
+	interleave = 4;
+	ymult = 3;
+	ydiv = 2;
+    }
+
+    /* CGA/EGA/VGA screen parameters */
+    else {
+	base = (char far *) 0xb8000000;
+	swidth = 80;
+	interleave = 2;
+	ymult = 1;
+	ydiv = 1;
+    }
+}
+
+/**
+ * Set hercules mode.
+ */
+static void set_hercules_mode (void)
+{
+    int i; /* index counter */
+    char params[12] /* register values */
+	= {0x35, 0x2d, 0x2e, 0x07, 0x5b, 0x02,
+	   0x57, 0x57, 0x02, 0x03, 0x00, 0x00};
+
+    /* set the mode and deactivate the screen */
+    outp (0x3bf, 3);
+    outp (0x3b8, 0xa /* 2 */);
+
+    /* set the registers */
+    for (i = 0; i < 12; ++i) {
+	outp (0x3b4, i);
+	outp (0x3b5, params[i]);
+    }
+
+    /* clear the screen */
+    _fmemset ((char far *) 0xb0000000, 0, 0x7fff);
+
+    /* activate the screen */
+    outp (0x3b8, 0xa);
+}
+
+/*----------------------------------------------------------------------
  * Level 1 Functions.
  */
+
+/**
+ * Attempt to detect Hercules Graphics Card.
+ * @return 1 if Hercules, 0 if not.
+ */
+static int is_hercules (void)
+{
+    union REGS regs;
+    int86 (0x11, &regs, &regs);
+    return ((regs.x.ax & 0x30) == 0x30);
+}
 
 /**
  * Set the display mode.
@@ -29,10 +120,19 @@ static void set_mode (Screen *screen)
     /* local variables */
     union REGS regs;
 
-    /* use the BIOS to set the mode. */
-    regs.h.ah = 0;
-    regs.h.al = screen->mode;
-    int86 (0x10, &regs, &regs);
+    /* set hercules mode */
+    if (screen->mode == 7)
+	set_hercules_mode ();
+
+    /* set bios-compatible modes */
+    else {
+
+	/* use the bios to select the mode */
+	regs.h.ah = 0;
+	regs.h.al = screen->mode;
+	int86 (0x10, &regs, &regs);
+
+    }
 }
 
 /**
@@ -48,7 +148,7 @@ static void palette_cga (Screen *screen)
     char colour_control;
 
     /* don't do any of this if we're in monochrome mode */
-    if (screen->mode == 6) return;
+    if (screen->mode != 4 && screen->mode != 5) return;
 
     /* set the mode according to the palette chosen */
     screen->mode = ((screen->palette % 3) == 2) ? 5 : 4;
@@ -98,7 +198,7 @@ static void palette_ega (Screen *screen)
     int fgcount; /* count of foreground colours */
 
     /* don't do any of this if we're in monochrome mode */
-    if (screen->mode == 6) return;
+    if (screen->mode != 4 && screen->mode != 5) return;
 
     /* background colour */
     regs.w.ax = 0x1000;
@@ -131,7 +231,7 @@ Screen *scr_create (int mode)
     /* initialise the screen parameters */
     if (! (screen = malloc (sizeof (Screen))))
         return NULL;
-    screen->mode = mode;
+    screen->mode = is_hercules () ? 7 : mode;
     screen->palette = (mode == 5) ? 5 : 4;
     screen->colour = 0;
     screen->ink = 3;
@@ -184,18 +284,25 @@ void scr_putpart (Screen *dst, Bitmap *src, int xd, int yd,
     char *s; /* address to copy data from */
     int r; /* row counter */
     int b; /* byte counter */
+    int ydc; /* destination y coordinate */
 
     /* dst is not used but here for future proofing */
     dst = dst; /* shut the compiler up, hopefully */
+
+    /* ensure the screen parameters are set up */
+    if (! ydiv)
+	set_screen_parameters (dst);
 
     /* copy the pixels */
     for (r = 0; r < h; ++r)
 
         /* DRAW_PSET can be copied by a quicker method */
         if (draw == DRAW_PSET) {
-            d = (r % 2)
-                ? xd / 4 + (yd + r - 1) * 40 + (char far *) 0xb8002000
-                : xd / 4 + (yd + r) * 40 + (char far *) 0xb8000000;
+	    ydc = (yd + r) * ymult / ydiv;
+	    d = xd / 4
+		+ 0x2000 * (ydc % interleave)
+		+ swidth * (ydc / interleave)
+		+ base;
             s = src->pixels + (xs / 4) + (src->width / 4) * (ys + r);
             _fmemcpy (d, s, w / 4);
         }
@@ -203,12 +310,12 @@ void scr_putpart (Screen *dst, Bitmap *src, int xd, int yd,
 	/* the other draw operations need doing byte by byte */
         else 
             for (b = 0; b < w / 4; ++b) {
-                d = b +
-		    ((r % 2)
-		     ? xd / 4 + (yd + r - 1) * 40
-		     + (char far *) 0xb8002000
-		     : xd / 4 + (yd + r) * 40
-		     + (char far *) 0xb8000000);
+		ydc = (yd + r) * ymult / ydiv;
+		d = b
+		    + xd / 4
+		    + 0x2000 * (ydc % interleave)
+		    + swidth * (ydc / interleave)
+		    + base;
                 s = b + src->pixels + (xs / 4)
 		    + (src->width / 4) * (ys + r);
                 switch (draw) {
@@ -243,18 +350,25 @@ void scr_put (Screen *dst, Bitmap *src, int x, int y, DrawMode draw)
     char *s; /* address to copy data from */
     int r; /* row counter */
     int b; /* byte counter */
+    int yd; /* destination y coordinate */
 
     /* dst is not used but here for future proofing */
     dst = dst; /* shut the compiler up, hopefully */
+
+    /* ensure the screen parameters are set up */
+    if (! ydiv)
+	set_screen_parameters (dst);
 
     /* copy the pixels */
     for (r = 0; r < src->height; ++r)
 
         /* DRAW_PSET can be copied by a quicker method */
         if (draw == DRAW_PSET) {
-            d = (r % 2)
-                ? x / 4 + (y + r - 1) * 40 + (char far *) 0xb8002000
-                : x / 4 + (y + r) * 40 + (char far *) 0xb8000000;
+	    yd = (y + r) * ymult / ydiv;
+	    d = x / 4
+		+ 0x2000 * (yd % interleave)
+		+ swidth * (yd / interleave)
+		+ base;
             s = src->pixels + src->width / 4 * r;
             _fmemcpy (d, s, src->width / 4);
         }
@@ -262,9 +376,11 @@ void scr_put (Screen *dst, Bitmap *src, int x, int y, DrawMode draw)
         /* the other draw operations need doing byte by byte */
         else 
             for (b = 0; b < src->width / 4; ++b) {
-                d = b + ((r % 2)
-                    ? x / 4 + (y + r - 1) * 40 + (char far *) 0xb8002000
-                    : x / 4 + (y + r) * 40 + (char far *) 0xb8000000);
+		yd = (y + r) * ymult / ydiv;
+		d = b + x / 4
+		    + 0x2000 * (yd % interleave)
+		    + swidth * (yd / interleave)
+		    + base;
                 s = b + src->pixels + src->width / 4 * r;
                 switch (draw) {
                     case DRAW_PRESET:
@@ -296,15 +412,22 @@ void scr_get (Screen *src, Bitmap *dst, int x, int y)
     char far *s; /* address to copy data from */
     char *d; /* address to copy data to */
     int r; /* row counter */
+    int ys; /* y source coordinate */
 
     /* src is here for future proofing */
     src = src; /* shut the compiler up, hopefully */
 
+    /* ensure the screen parameters are set up */
+    if (! ydiv)
+	set_screen_parameters (src);
+
     /* copy the pixels */
     for (r = 0; r < dst->height; ++r) {
-        s = (r % 2)
-            ? x / 4 + (y + r - 1) * 40 + (char far *) 0xb8002000
-            : x / 4 + (y + r) * 40 + (char far *) 0xb8000000;
+ 	ys = (y + r) * ymult / ydiv;
+	s = x / 4
+	    + 0x2000 * (ys % interleave)
+	    + swidth * (ys / interleave)
+	    + base;
         d = dst->pixels + dst->width / 4 * r;
         _fmemcpy (d, s, dst->width / 4);
     }
@@ -326,15 +449,22 @@ void scr_box (Screen *screen, int x, int y, int width, int height)
     char v; /* value to write to the screen */
     int r; /* row counter */
     char far *d; /* address to write to */
+    int yd; /* y destination coordinate */
+
+    /* ensure the screen parameters are set up */
+    if (! ydiv)
+	set_screen_parameters (screen);
 
     /* determine the byte value that will fill the box */
     v = screen->ink * 0x55;
 
     /* fill each individual row */
     for (r = 0; r < height; ++r) {
-        d = (r % 2)
-            ? x / 4 + (y + r - 1) * 40 + (char far *) 0xb8002000
-            : x / 4 + (y + r) * 40 + (char far *) 0xb8000000;
+	yd = (y + r) * ymult / ydiv;
+	d = x / 4
+	    + 0x2000 * (yd % interleave)
+	    + swidth * (yd / interleave)
+	    + base;
         _fmemset (d, v, width / 4);
     }
 }
@@ -352,18 +482,25 @@ void scr_print (Screen *screen, int x, int y, char *message)
     /* local variables */
     int b; /* character pointer */
     int r; /* row of character */
+    int yd; /* destination y coordinate */
     char v; /* value of byte in character */
     char far *d; /* pointer to destination byte on screen */
 
     /* only try to print if a font is selected */
     if (! screen->font) return;
 
+    /* ensure the screen parameters are set up */
+    if (! ydiv)
+	set_screen_parameters (screen);
+
     /* print each character */
     for (b = 0; message[b]; ++b)
         for (r = 0; r < 8; ++r) {
-            d = b + ((r % 2)
-                ? x / 4 + (y + r - 1) * 40 + (char far *) 0xb8002000
-                : x / 4 + (y + r) * 40 + (char far *) 0xb8000000);
+	    yd = (y + r) * ymult / ydiv;
+	    d = b + x / 4
+		+ 0x2000 * (yd % interleave)
+		+ swidth * (yd / interleave)
+		+ base;
             v = screen->font->pixels[r + 8
                 * (message[b] - screen->font->first)];
             if (screen->ink != 3 || screen->paper != 0)
